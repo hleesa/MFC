c866ac7436aed967bea7de0caf8082b133388d6b
hleesa <lshhi100@gmail.com>
2024-10-17 13:52:06 +0900

    MFC start

---
+++ MFCStart2/MFCStart2/MFCStart2Dlg.h
@@ -0,0 +1,38 @@
+﻿
+// MFCStart2Dlg.h: 헤더 파일
+//
+
+#pragma once
+
+
+// CMFCStart2Dlg 대화 상자
+class CMFCStart2Dlg : public CDialogEx
+{
+// 생성입니다.
+public:
+	CMFCStart2Dlg(CWnd* pParent = nullptr);	// 표준 생성자입니다.
+
+// 대화 상자 데이터입니다.
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MFCSTART2_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 지원입니다.
+
+
+// 구현입니다.
+protected:
+	HICON m_hIcon;
+
+	// 생성된 메시지 맵 함수
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedBtnTest();
+	int m_nNum;
+	int m_nNumRhs;
+};
